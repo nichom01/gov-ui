83306b626b62c1eddeec4402ab0d0b292b8ab156
nichom01 <martin@neversoft.co.uk>
2026-02-03 13:53:47 +0000

    Add navigation component to Home and About pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,21 +1,31 @@
+import Navigation from '../components/Navigation'
+
 function About() {
+  const navigationItems = [
+    { href: '/', text: 'Home' },
+    { href: '/about', text: 'About', current: true },
+  ]
+
   return (
-    <div className="govuk-width-container" style={{ flex: '1' }}>
-      <main className="govuk-main-wrapper" id="main-content">
-        <h1 className="govuk-heading-xl">About</h1>
-        <p className="govuk-body-l">
-          Learn more about this application.
-        </p>
-        <p className="govuk-body">
-          This is a GOV.UK styled application built with React and TypeScript.
-        </p>
-        <h2 className="govuk-heading-l">Purpose</h2>
-        <p className="govuk-body">
-          This template provides a foundation for building accessible, user-friendly
-          government services that follow the GOV.UK Design System.
-        </p>
-      </main>
-    </div>
+    <>
+      <Navigation navigation={navigationItems} />
+      <div className="govuk-width-container" style={{ flex: '1' }}>
+        <main className="govuk-main-wrapper" id="main-content">
+          <h1 className="govuk-heading-xl">About</h1>
+          <p className="govuk-body-l">
+            Learn more about this application.
+          </p>
+          <p className="govuk-body">
+            This is a GOV.UK styled application built with React and TypeScript.
+          </p>
+          <h2 className="govuk-heading-l">Purpose</h2>
+          <p className="govuk-body">
+            This template provides a foundation for building accessible, user-friendly
+            government services that follow the GOV.UK Design System.
+          </p>
+        </main>
+      </div>
+    </>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,26 @@
+import Navigation from '../components/Navigation'
+
 function Home() {
+  const navigationItems = [
+    { href: '/', text: 'Home', current: true },
+    { href: '/about', text: 'About' },
+  ]
+
   return (
-    <div className="govuk-width-container" style={{ flex: '1' }}>
-      <main className="govuk-main-wrapper" id="main-content">
-        <h1 className="govuk-heading-xl">Welcome to GOV.UK</h1>
-        <p className="govuk-body-l">
-          This is the home page of your application.
-        </p>
-        <p className="govuk-body">
-          You can use this template to build your GOV.UK styled application.
-        </p>
-      </main>
-    </div>
+    <>
+      <Navigation navigation={navigationItems} />
+      <div className="govuk-width-container" style={{ flex: '1' }}>
+        <main className="govuk-main-wrapper" id="main-content">
+          <h1 className="govuk-heading-xl">Welcome to GOV.UK</h1>
+          <p className="govuk-body-l">
+            This is the home page of your application.
+          </p>
+          <p className="govuk-body">
+            You can use this template to build your GOV.UK styled application.
+          </p>
+        </main>
+      </div>
+    </>
   )
 }
 
